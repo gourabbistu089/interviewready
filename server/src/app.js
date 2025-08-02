2d92f9f60a4dc387129c4372ff7caa20d509ad91
Gourab <gourabbistu2002@gmail.com>
2025-08-02 17:57:03 +0530

    add frontedurl for avoiding cors issue

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,7 @@ const app = express();
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:5174','http://localhost:5173'],
+  origin: ['http://localhost:5174','http://localhost:5173',process.env.FRONTEND_URL],
   credentials: true
 }));
 
