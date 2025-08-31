b4f491a28d280542d1fd011e04083ad477f37e77
anas_30 <162482722+anasakhtar30@users.noreply.github.com>
2025-09-01 00:03:04 +0530

    ttl implemented on user

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,142 +1,4 @@
-// const mongoose = require("mongoose");
-// const bcrypt = require("bcryptjs");
 
-// const userSchema = new mongoose.Schema({
-//   username: {
-//     type: String,
-//     required: [true, "Username is required"],
-//     unique: true,
-//     trim: true,
-//     minlength: [3, "Username must be at least 3 characters long"],
-//   },
-//   email: {
-//     type: String,
-//     required: [true, "Email is required"],
-//     unique: true,
-//     lowercase: true,
-//     match: [
-//       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-//       "Please enter a valid email",
-//     ],
-//   },
-//   password: {
-//     type: String,
-//     required: [true, "Password is required"],
-//     minlength: [6, "Password must be at least 6 characters long"],
-//   },
-//   firstName: {
-//     type: String,
-//     required: [true, "First name is required"],
-//     trim: true,
-//   },
-//   lastName: {
-//     type: String,
-//     required: [true, "Last name is required"],
-//     trim: true,
-//   },
-//   profilePicture: {
-//     type: String,
-    
-//   },
-//   role: {
-//     type: String,
-//     enum: ["user", "admin"],
-//     default: "user",
-//   },
-//   // field for dashboard
-//   stats: {
-//     questionsCompleted: { type: Number, default: 0 },
-//     mockInterviews: { type: Number, default: 0 },
-//     blogsWritten: { type: Number, default: 0 },
-//     modulesCompleted: { type: Number, default: 0 },
-//   },
-
-//   // recent activity
-//   activity: {
-//     latestBlog: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "Blog",
-//       default: null,
-//     },
-//     latestSubtopic: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "Subtopic",
-//       default: null,
-//     },
-//     latestInterviewSession: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "InterviewSession",
-//       default: null,
-//     },
-//     latestQuestion: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "Question",
-//       default: null,
-//     },
-//   },
-//   revisionQuestions: [
-//     { type: mongoose.Schema.Types.ObjectId, 
-//       ref: "Question" ,
-//     },
-//   ],
-
-//   isActive: {
-//     type: Boolean,
-//     default: true,
-//   },
-//   lastLogin: {
-//     type: Date,
-//     default: Date.now,
-//   },
-//   createdAt: {
-//     type: Date,
-//     default: Date.now,
-//   },
-//   updatedAt: {
-//     type: Date,
-//     default: Date.now,
-//   },
-// });
-
-// // Hash password before saving
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified("password")) return next();
-
-//   try {
-//     const salt = await bcrypt.genSalt(10);
-//     this.password = await bcrypt.hash(this.password, salt);
-//     next();
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// // Update updatedAt field before saving
-// userSchema.pre("save", function (next) {
-//   this.updatedAt = Date.now();
-//   next();
-// });
-
-// // Compare password method
-// userSchema.methods.comparePassword = async function (candidatePassword) {
-//   return await bcrypt.compare(candidatePassword, this.password);
-// };
-
-// // Get full name
-// userSchema.virtual("fullName").get(function () {
-//   return `${this.firstName} ${this.lastName}`;
-// });
-
-// // Ensure virtual fields are serialized
-// userSchema.set("toJSON", {
-//   virtuals: true,
-//   transform: function (doc, ret) {
-//     delete ret.password;
-//     return ret;
-//   },
-// });
-
-// module.exports = mongoose.model("User", userSchema);
 
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
@@ -304,5 +166,10 @@ userSchema.set("toJSON", {
     return ret;
   },
 });
+
+userSchema.index(
+  { createdAt: 1 },
+  { expireAfterSeconds: 60, partialFilterExpression: { isVerified: false } }
+);
 
 module.exports = mongoose.model("User", userSchema);
